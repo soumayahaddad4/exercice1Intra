80adfce8a866bab158a36411a49f08acaa7f6b06
soumaya <soumaya1.haddad@gmail.com>
2023-06-10 02:23:43 -0400

    modification apportés apres l'examen

--- exercice1.py
+++ exercice1.py
@@ -4,7 +4,6 @@ import pytest
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as ec  # on a utilisé l'alias
 from selenium.webdriver.support.wait import WebDriverWait
 
 driver = webdriver.Chrome()
@@ -12,7 +11,7 @@ driver.maximize_window()
 
 wait=WebDriverWait(driver,10)  #l'explicite wait prend comme parametre le driver et le temps limite d'attente
 
-def test_iframe():
+def test_ex1():
     driver.get("https://videotron.com/")
     driver.maximize_window()
     #images du site
@@ -26,13 +25,15 @@ def test_iframe():
     liste_lien=driver.find_elements(By.TAG_NAME,"a")
     print("le nombre total des liens =  ", len(liste_lien))
     for lien in liste_lien:
-        url= lien.get_attribute('href')
-        print(url)
+        print(lien.get_attribute("href"))
+
     #lien du footer
     print("**************************************************************************************************")
-    lien_footer=driver.find_elements(By.XPATH,"//footer//span | //footer//a")
+    footer = driver.find_element(By.TAG_NAME,"footer")
+    lien_footer=footer.find_elements(By.TAG_NAME,"a")
     print("le nombre total des liens dans le footer=  ", len(lien_footer))
+
     for lien in lien_footer:
-        url=lien.get_attribute('href')
-        print(url)
+        print(lien.get_attribute('href'))
+
     driver.quit()
